0ead8927c72bacd48e385012c98d1f832a9d2ca7
Patrick DeVries <pdevries@smu.edu>
2021-03-30 22:37:02 -0500

    added movement

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
 import * as THREE from './three/src/Three.js';
-// import {Scene} from './three/Scene.js'
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, .01, 1000);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-
-// var loader = new GLTFLoader();
-
-// var obj;
-// loader.load("me.gltf", function(gltf){
-//     scene.add(gltf.scene);
-//     obj = gltf.scene;
-// })
-
 
 // define cube
 var geometry = new THREE.BoxGeometry();
@@ -27,27 +17,103 @@ scene.add(light);
 
 camera.position.set(0, 1, 3);
 
+
+// track if the mouse button is held down
+var mouseDown = false;
+document.body.onmousedown = function() { 
+    mouseDown = true;
+}
+document.body.onmouseup = function() {
+    mouseDown = false;
+}
+
+// track if the motion keys are held down
+var up = false;
+var down = false;
+var left = false;
+var right = false;
+
+function pressKey() {
+    if (event.keyCode === 87) {
+        up = true;
+    }
+    else if (event.keyCode === 83) {
+        down = true;
+    }
+    else if (event.keyCode === 65) {
+        left = true;
+    }
+    else if (event.keyCode === 68) {
+        right = true;
+    }
+    console.log(event.keyCode);
+}
+document.onkeydown = pressKey;
+
+// track if the motion keys are released
+function releaseKey() {
+    if (event.keyCode === 87) {
+        up = false;
+    }
+    else if (event.keyCode === 83) {
+        down = false;
+    }
+    else if (event.keyCode === 65) {
+        left = false;
+    }
+    else if (event.keyCode === 68) {
+        right = false;
+    }
+}
+document.onkeyup = releaseKey;
+
+
+// track mouse change and update location
 var mx = 0;
 var my = 0;
+var lmx = 0;
+var lmy = 0;
 function saveMouse() {
-    mx = event.clientX;
-    my = event.clientY;
-
+    let dx = event.clientX - lmx;
+    let dy = event.clientY - lmy;
+    lmx = event.clientX;
+    lmy = event.clientY;
+    if (mouseDown) {
+        my = my + dy;
+        mx = mx + dx;
+    }
 }
 document.onmousemove = saveMouse;
 
-// scene.background= new THREE.Color(0x0000FF)
+var direction = new THREE.Vector3;
+var speed = 0.1;
 
 function animate() {
     requestAnimationFrame(animate);
-
-    cube.rotation.y = mx/500;
-    cube.rotation.x = my/500;
-
-    renderer.render(scene, camera);
+    // update  background color
     chrome.storage.sync.get("color", ({ color }) => {
         scene.background= new THREE.Color(color);
         ;
       });
+
+
+    // handle movement
+    if (up) {
+        camera.getWorldDirection(direction);
+        camera.position.addScaledVector(direction, speed);
+    }
+    if (down) {
+        camera.getWorldDirection(direction);
+        camera.position.addScaledVector(direction, -speed);
+    }
+
+    
+    // handle rotation
+    camera.rotation.y = mx/250;
+
+    camera.rotation.x = my/250;
+
+
+    renderer.render(scene, camera);
 }
 animate();
